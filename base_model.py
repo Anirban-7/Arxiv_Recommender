f5b548e0a1cda48901f1d04c221156c6df3a1e01
Michael-Geis <leamy1@gmail.com>
2023-05-26 21:29:23 -0400

    Editing base model recommend method

--- base_model.py
+++ base_model.py
@@ -13,7 +13,7 @@ class Recommender:
         pass
 
     
-    def load_library(self,path_to_library,path_to_vectorized,vectorizer):
+    def load_library(self,path_to_library,path_to_vectorized,vectorizer,vectorizer_kwargs=None):
         """Loads the library from which we will recommend. Included so as to make enlarging the library
         easier if desired. Assumes the library has been vectorized separately.
 
@@ -23,10 +23,13 @@ class Recommender:
             vectorizer: object which was used to transform the library text to vectors. Can be any type, e.g.
             a sentence transformer, tfidf scheme, word2vec embedding, etc. Must be able to take in a string
             and return its embedding.
+            vectorizer_kwargs: dictionary consisting of additional keyword arguments to be passed to vectorizer
+            when called on an input to the 'recommend' method.
         """
         self.vectorizer = vectorizer
         self.library = pd.read_parquet(path_to_library) #? Do we want to load only certain cols here
         self.vectorized_library = np.loadtxt(path_to_vectorized)
+        self.vectorizer_kwargs = vectorizer_kwargs
 
     def load_topic_models(self,path_to_models):
         """Creates a dictionary whose item keys are the file names for each of our topic models and whose
@@ -36,7 +39,7 @@ class Recommender:
             path_to_models: file path to the folder containing the topic models.
         """
 
-        #TODO: Adapt to handle other topic models
+        #TODO: Adapt to handle other topic models?
 
         model_files = glob.glob(os.path.join(path_to_models,"*"))
         self.topic_models = {f.split("\\")[-1] : BERTopic.load(f) for f in model_files}
@@ -44,8 +47,8 @@ class Recommender:
         
 
     def _get_topic_model(self,input):
-        #* Placeholder for a function that finds the correct topic model to use for a given input paper.
-        pass
+        #### PLACEHOLDER THAT JUST RETURNS THE SINGLE EXAMPLE TOPIC MODEL.
+        return self.topic_models['test_topic_model']
 
     def recommend(self,arxiv_id,use_topics=False):
         """Yields the top 5 most similar articles to an arxiv article input by the user.
@@ -54,74 +57,77 @@ class Recommender:
             arxiv_id: valid arXiv id number.
 
         Returns:
-            Top 5 most similar articles in the library.
+            Top 5 most similar articles in the library in a pandas dataframe.
         """
 
         #### FIRST RETRIEVE, CLEAN, AND VECTORIZE INPUT
 
         #* Retrieve the input metadata
         input = next(arxiv.Search(id_list=[arxiv_id]).results())
-        input_metadata = vars(input)
         
         #* Clean the title, abstract, categories, and authors
-        input['summary'] = data_utils.clean(input['summary'])
-        input['title'] = data_utils.clean(input['title'])
-        input['math_categories'] = data_utils.clean_cat_list(input['math_categories'])
-        #* clean authors here -- input['authors']
+        input_abs = data_utils.clean_data(input.summary)
+        input_title = input.title
+        input_cats = data_utils.clean_cat_list(input.categories)
+        input_authors = [author.name for author in input.authors]
 
         #* Transform the input abstract
-        #! MUST RETURN 2-DIM PYTORCH TENSOR!
-        input_sentence = self.vectorizer(input['summary'])
-        
+        input_embedding = self.vectorizer(input_abs,**self.vectorizer_kwargs)
+        #* VECTORIZER = SENTENCE TRANSFORMER, OUTPUT IS A 1D NP ARRAY
         ####
 
         #### SEARCH LIBRARY FOR TOP 5 MOST SIMILAR ABSTRACTS
 
-        #### NAIVE SEARCH WITHOUT TOPIC INFO
+        #### NAIVE SEARCH FOR TOP K=5 MOST SIMILAR WITHOUT TOPIC INFO
         if not use_topics:
             
-            top_5_indices = sentence_transformers.util.semantic_search(
-                query_embeddings=input_sentence,
+            recs = sentence_transformers.util.semantic_search(
+                query_embeddings=input_embedding,
                 corpus_embeddings=self.vectorized_library,
                 top_k=5
                 )
-            top_5_papers = self.library.iloc[top_5_indices]
+            rec_inds = [rec['corpus_id'] for rec in recs[0]]
+            rec_scores = [rec['score'] for rec in recs[0]]
         
-            return top_5_papers
+        #### RETURN ROWS OF LIBRARY CORRESPONDING TO RECS WITH SCORES
+            rec_df = self.library.iloc[rec_inds]
+            rec_df['score'] = pd.Series(rec_scores, index=rec_inds)
+
+            return rec_df
         ####
 
         else:
 
         #### FIRST FIND THE DOCS IN SIMILAR TOPICS
-
-        #* Select the appropriate topic model for this input. Predict the topic probabilities.
-            input_topic_model = self._get_topic_model(input)
-            _ , input_probs = input_topic_model.transform(input_sentence)
-
-            topic_indices = np.argpartition(input_probs[0], -3)[-3:] #* Returns the top 3 most likely topics
-
-        #* Retrieve the indicies of the documents contained within these topics
-
-        doc_indicies = self.library.loc[self.library['topic_labels'].isin(topic_indices)].index
         
-        #* Retrieve the corresponding embeddings
+            #### GET CORRECT TOPIC MODEL
+            input_topic_model = self._get_topic_model(input)
+            _ , input_probs = input_topic_model.transform(input_embedding)
 
-        embeddings = self.vectorized_library[doc_indicies]    
-        ####
+            #### RETURNS 3 MOST LIKELY TOPICS
+            topic_indices = np.argpartition(input_probs[0], -3)[-3:] 
+
+            #### GET ALL DOCS IN THESE TOPICS
+            doc_indicies = self.library.loc[self.library['topic_labels'].isin(topic_indices)].index
+        
+            #### GET EMBEDDINGS
+            embeddings = self.vectorized_library[doc_indicies]    
+            ####
 
         #### NOW SEARCH FOR TOP 5 SIMILAR ABSTRACTS WITHIN THIS SUBSET
-        if not use_topics:
-            
-            top_5_indices = sentence_transformers.util.semantic_search(
-                query_embeddings=input_sentence,
-                corpus_embeddings=embeddings,
-                top_k=5
-                )
-            top_5_papers = self.library.iloc[top_5_indices]
+            recs = sentence_transformers.util.semantic_search(
+            query_embeddings=input_embedding,
+            corpus_embeddings=embeddings,
+            top_k=5
+            )
+    
+            rec_inds = [rec['corpus_id'] for rec in recs[0] ]
+            rec_scores = [rec['score'] for rec in recs[0]]
         
-            return top_5_papers
+            rec_df = self.library.iloc[rec_inds]
+            rec_df['score'] = pd.Series(rec_scores, index=rec_inds)
+
+            return rec_df
         ####
-
-
 
 
